e574eb588073a4e521e309ab643f62688650b371
PlaustralCL <plaustralcl@gmail.com>
2026-05-27 20:33:16 -0400

    Refactor date labeling

--- weekly.js
+++ weekly.js
@@ -1,3 +1,20 @@
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
+mondayFullDate = new Date();
+
 function buildMonthTable() {
   const tableBody = document.getElementById("table-body");
   for (let row = 1; row <= 6; row++) {
@@ -37,27 +54,55 @@ function populateMonthDates() {
   }
 }
 
+function setMondayDate(startDate) {
+  console.log(startDate);
+  mondayFullDate = startDate;
+  day = mondayFullDate.getDay();
+  if (day === 0) {
+    mondayFullDate.setDate(startDate.getDate() - 6);
+  } else if (day != 1) {
+    mondayFullDate.setDate(startDate.getDate() - (day - 1));
+  }
+}
+
+function setMonthHeaders() {
+  mondayMonth = mondayFullDate.getMonth();
+  mondayFullYear = mondayFullDate.getFullYear();
+  headerText = `${months[mondayMonth]} ${mondayFullYear}`;
+  header1 = document.getElementById("header-month-1");
+  header1.textContent = headerText;
+  header2 = document.getElementById("header-month-2");
+  header2.textContent = headerText;
+}
+
+function setWeeklyDates() {
+  // Add dates to the days in the weekly lines
+  workingDate = mondayFullDate;
+  console.log(startDate);
+  for (i = 1; i < 8; i++) {
+    const dayId = document.getElementById(`day${i % 7}`);
+    currentValue = dayId.textContent;
+    dayId.textContent = `${workingDate.getDate()} ${currentValue}`;
+    workingDate.setDate(workingDate.getDate() + 1);
+  }
+}
+
+function addWeek() {
+
+}
+
+function subtractWeek() {
+
+}
+
 window.onload = function () {
   //console.log("loaded!");
   //buildMonthTable();
   //populateMonthDates();
+
   startDate = new Date(2026, 4, 22); // 5/22/26
-  console.log(startDate);
-  day = startDate.getDay();
-  if (day === 0) {
-    startDate.setDate(startDate.getDate() - 6);
-  } else if (day != 1) {
-    startDate.setDate(startDate.getDate() - (day - 1));
-  }
 
-  workingDate = startDate;
-  console.log(startDate);
-  for (i = 1; i < 8; i++) {
-    const dayId = document.getElementById(`day${i%7}`);
-    currentValue = dayId.textContent;
-    dayId.textContent = `${workingDate.getDate()} ${currentValue}`;
-    workingDate.setDate(workingDate.getDate() + 1);
-
-  }
-
+  setMondayDate(startDate);
+  setMonthHeaders();
+  setWeeklyDates();
 };
