1190e1cc55fcd2c5a933cc3f12ea0b9cbb1f105e
PlaustralCL <plaustralcl@gmail.com>
2026-05-29 20:04:00 -0400

    Add mini calendars

--- weekly.js
+++ weekly.js
@@ -15,14 +15,14 @@ const months = [
 
 mondayFullDate = new Date();
 
-function buildMonthTable() {
-  const tableBody = document.getElementById("table-body");
+function buildMonthTable(calendarId) {
+  const tableBody = document.getElementById(`${calendarId}-table-body`);
   for (let row = 1; row <= 6; row++) {
     const newRow = document.createElement("tr");
-    newRow.id = `row${row}`;
+    newRow.id = `row${calendarId}${row}`;
     for (let col = 0; col < 7; col++) {
       const newCell = document.createElement("td");
-      const cellId = `cell${row}${col}`;
+      const cellId = `cell${calendarId}${row}${col}`;
       newCell.id = cellId;
       newCell.textContent = cellId;
       newRow.append(newCell);
@@ -31,19 +31,22 @@ function buildMonthTable() {
   }
 }
 
-function populateMonthDates() {
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = date.getMonth();
+function populateMonthDates(calendarId) {
+  const year = mondayFullDate.getFullYear();
+  let month = mondayFullDate.getMonth();
+  if (calendarId === "calendar2") {
+    month += 1;
+  }
   const startDate = new Date(year, month, 1);
+  console.log(`${calendarId}: ${month}`);
 
-  const monthHeader = document.getElementById("month-header");
-  monthHeader.textContent = date.toLocaleString("default", { month: "long" });
+  const monthHeader = document.getElementById(`${calendarId}-header`);
+  monthHeader.textContent = startDate.toLocaleString("default", { month: "long" });
 
   let workingDate = startDate;
   for (let row = 1; row <= 6; row++) {
     for (let cell = 0; cell < 7; cell++) {
-      const currentCell = document.getElementById(`cell${row}${cell}`);
+      const currentCell = document.getElementById(`cell${calendarId}${row}${cell}`);
       if (month === workingDate.getMonth() && workingDate.getDay() === cell) {
         currentCell.textContent = workingDate.getDate();
         workingDate.setDate(workingDate.getDate() + 1);
@@ -83,9 +86,7 @@ function setMonthHeaders() {
 function setWeeklyDates() {
   // Add dates to the days in the weekly lines
   //workingDate = mondayFullDate;
-  workingDate = new Date(mondayFullDate.getFullYear(),
-                         mondayFullDate.getMonth(),
-                         mondayFullDate.getDate());
+  workingDate = new Date(mondayFullDate.getFullYear(), mondayFullDate.getMonth(), mondayFullDate.getDate());
   for (i = 1; i < 8; i++) {
     const dayId = document.getElementById(`day${i % 7}`);
     currentValue = dayId.textContent;
@@ -99,14 +100,19 @@ function addWeek() {
   mondayFullDate.setDate(mondayFullDate.getDate() + 7);
   setMonthHeaders();
   setWeeklyDates();
+
+  populateMonthDates("calendar1");
+  populateMonthDates("calendar2");
 }
 
 function subtractWeek() {
   mondayFullDate.setDate(mondayFullDate.getDate() - 7);
   setMonthHeaders();
   setWeeklyDates();
-}
 
+  populateMonthDates("calendar1");
+  populateMonthDates("calendar2");
+}
 
 window.onload = function () {
   //console.log("loaded!");
@@ -118,4 +124,10 @@ window.onload = function () {
   setMondayDate(startDate);
   setMonthHeaders();
   setWeeklyDates();
+
+  buildMonthTable("calendar1");
+  populateMonthDates("calendar1");
+
+  buildMonthTable("calendar2");
+  populateMonthDates("calendar2");
 };
